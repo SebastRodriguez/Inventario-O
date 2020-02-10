a65a3c8cf423089dc71a9d06024de44aaa9e46a4
Sebastian Rodriguez <58979459+SebastRodriguez@users.noreply.github.com>
2020-02-10 09:16:12 -0300

    Delete VMOFF.ps1

--- VMOFF.ps1
+++
@@ -1,9 +0,0 @@
-  
-$subscriptionName = Get-AutomationVariable -Name "SubscriptionName"
-$cred = Get-AutomationPSCredential -Name "automationCred"
-
-Login-AzureRmAccount -Credential $cred
-Select-AzureRmSubscription -SubscriptionName $subscriptionName
-
-Stop-AzureRmVM -ResourceGroupName "Canales-NOVT" -Name "ORI-DEV-APPSW" -Force
-Stop-AzureRmVM -ResourceGroupName "Canales-NOVT" -Name "ORI-DEV-WEBSW" -Force
